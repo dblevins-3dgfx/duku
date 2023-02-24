4fb5e61a4c7a68482445cf70b0e36c43358b5281
David Blevins <31944327+dblevins-3dgfx@users.noreply.github.com>
2023-02-24 00:09:30 +0000

    Factor out some common functionality

--- duku.py
+++ duku.py
@@ -18,21 +18,31 @@ class Duku:
             rot = 3*i + int(i/3)
             self.sequentialize_row(row, rot)
 
-    def row_values(self, row_idx):
-        return [square.value for square in self.board[row_idx] if square.value is not None]
-
-    def col_values(self, col_idx):
-        return [row[col_idx].value for row in self.board if row[col_idx].value is not None]
-
-    def block_values(self, block_coord):
+    def get_row(self, row_idx):
+        return [square for square in self.board[row_idx]]
+    
+    def get_col(self, col_idx):
+        return [row[col_idx] for row in self.board]
+    
+    def get_block(self, block_coord):
         result = []
         base = self.board[int(block_coord[0]/3)][int(block_coord[1]/3)].coord
         for i in range(3):
             for j in range(3):
-                val = self.board[base[0]+i][base[1]+j].value
-                if (val is not None):
-                    result.append(val)
+                result.append(self.board[base[0]+i][base[1]+j])
         return result
+
+    def values(self, squares):
+        return [square.value for square in squares if square.value is not None]
+
+    def row_values(self, row_idx):
+        return self.values(self.get_row(row_idx))
+
+    def col_values(self, col_idx):
+        return self.values(self.get_col(col_idx))
+
+    def block_values(self, block_coord):
+        return self.values(self.get_block(block_coord))
 
     def check_unique(self, values):
         value_set = set(values)
